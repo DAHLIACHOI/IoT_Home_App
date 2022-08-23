313cd80f41fec1ae68cf859e5f60c596815acb43
DAHLIACHOI <cyj000424@naver.com>
2022-08-23 23:38:40 +0900

    login

--- app/src/main/java/com/example/iotapp/LoginActivity.java
+++ app/src/main/java/com/example/iotapp/LoginActivity.java
@@ -2,13 +2,28 @@ package com.example.iotapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_register = (Button) findViewById(R.id.btn_register);
+
+        btn_register.setOnClickListener (new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class); //LoginActivity에서 RegisterActivity로 이동
+                startActivity(intent);
+            }
+        });
     }
 }
