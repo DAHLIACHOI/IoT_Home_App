6ff6d987c2a8e74c81c0e0c3de91a22b24b936c0
DAHLIACHOI <cyj000424@naver.com>
2022-09-06 00:06:02 +0900

    temp/hum

---
+++ app/src/main/java/com/example/iotapp/TempHumActivity.java
@@ -0,0 +1,20 @@
+package com.example.iotapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class TempHumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp_hum);
+    }
+
+
+    public void goToHome(View view) {   //뒤로가기 버튼 클릭 시
+        finish();   //현재 액티비티 없애기
+    }
+}
