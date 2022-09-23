e5672aee7bafc85f474affce62b8684e3fbf08a8
ICDISSO <kim300255@gmail.com>
2022-09-24 01:49:34 +0900

    After modify, commit

--- app/src/main/java/com/example/iotapp/Money.java
+++ app/src/main/java/com/example/iotapp/Money.java
@@ -3,10 +3,18 @@ package com.example.iotapp;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class Money extends AppCompatActivity {
+
+   // final TextView todayDate = (TextView) findViewById(R.id.todayDate);
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -24,6 +32,31 @@ public class Money extends AppCompatActivity {
         setContentView(R.layout.activity_money);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextView todayDate = (TextView) findViewById(R.id.todayDate);
+        TextView monthDate = (TextView) findViewById(R.id.monthDate);
+
+        long now = System.currentTimeMillis(); // 1970년 1월 1일부터 몇 밀리세컨드가 지났는지를 반환함
+        Date date = new Date(now);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy년MM월dd일");//형식 지정
+        String getTime1 = simpleDateFormat.format(date);
+
+        todayDate.setText(getTime1);
+
+        //현재 월만 가져오기
+        Date currentTime = Calendar.getInstance().getTime();
+        SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy", Locale.getDefault());
+        SimpleDateFormat monthFormat = new SimpleDateFormat("MM", Locale.getDefault());
+
+        String year = yearFormat.format(currentTime);
+        String month = monthFormat.format(currentTime);
+
+        //현재 월의 마지막 일 가져오기
+        Calendar cal = Calendar.getInstance(); //캘린더 인스턴스 생성
+        cal.set(Integer.parseInt(year), Integer.parseInt(month)-1, 1); //캘린더에 기준이 되는 날짜(년, 월, 일) 입력, 0이 1월로 잡히기 때문에 월 -1
+        //따라서 월이 11(12월)을 넘어가면 년도도 알아서 넘어감
+
+        monthDate.setText(year+"년"+month+"월1일~"+cal.getActualMaximum(Calendar.DAY_OF_MONTH)+"일");
     }
 
     public void goToHome(View view) {   //뒤로가기 버튼 클릭 시
